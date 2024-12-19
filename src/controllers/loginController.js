b7d52374f1b16c5792036bd14bf8076041112aea
grzegorzadamek <grzegorzadamek@outlook.com>
2024-12-19 18:44:57 +0100

    add files structure -fix

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -34,19 +34,93 @@ const getAllItems = async (req, res) => {
 };
 
 const getItemById = async (req, res) => {
-    // kod z app.get('/item/:itemId')
+    const client = new pg.Client(config);
+    var itemId = req.params.itemId;
+    console.log('Get itemId :: ', itemId);
+    client.connect(function (err) {
+        if (err)
+            throw err;
+        client.query("SELECT * FROM logins WHERE id = $1", [itemId], function (err, result) {
+            const response = result.rows.map(({
+              first_name: firstName,
+              last_name: lastName,
+              ...rest
+            }) => ({
+              firstName,
+              lastName,
+              ...rest
+            }));
+
+            console.log(response[0]);
+            res.json(response[0]);
+            client.end(function (err) {
+                if (err)
+                    throw err;
+            });
+        });
+    });
 };
 
 const addItem = async (req, res) => {
-    // kod z app.post('/item/add')
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    console.log('Add item :: ', req.body);
+
+    const client = new pg.Client(config);
+    try {
+        await client.connect();
+        const result = await client.query(
+            "INSERT INTO logins (first_name, last_name, email) VALUES ($1, $2, $3) RETURNING *",
+            [firstName, lastName, email]
+        );
+        res.status(200).json("Add item successfully");
+    } catch (err) {
+        console.log(err);
+        res.status(500).json("Error adding item");
+    } finally {
+        await client.end();
+    }
 };
 
 const updateItem = async (req, res) => {
-    // kod z app.put('/item')
+    const id = req.body.id;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    console.log('Update item :: ', req.body);
+
+    const client = new pg.Client(config);
+    try {
+        await client.connect();
+        const result = await client.query(
+            "UPDATE logins SET first_name = $1, last_name = $2, email = $3 WHERE id = $4 RETURNING *",
+            [firstName, lastName, email, id]
+        );
+        res.status(200).json("Update item successfully");
+    } catch (err) {
+        console.log(err);
+        res.status(500).json("Error updating item");
+    } finally {
+        await client.end();
+    }
 };
 
 const deleteItem = async (req, res) => {
-    // kod z app.delete('/item/:itemId')
+    const client = new pg.Client(config);
+    var itemId = req.params.itemId;
+    console.log('Delete itemId :: ', itemId);
+    client.connect(function (err) {
+        if (err)
+            throw err;
+        client.query("DELETE FROM logins WHERE id = $1", [itemId], function (err, result) {
+            res.status(200).json("Remove item successfully!");
+            client.end(function (err) {
+                if (err)
+                    throw err;
+            });
+        });
+    });
 };
 
 module.exports = {
